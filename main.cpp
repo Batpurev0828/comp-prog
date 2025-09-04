d68db7a31e721e4f955bbab119e6342c1275d951
Batpurev0828 <sitoshi718@gmail.com>
2025-09-04 08:33:54 +0800

    setup

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cout << 123 << '\n';
+}
