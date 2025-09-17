13eacf730b4c74e7195efd01c2ccc6f38d086101
Batpurev0828 <sitoshi718@gmail.com>
2025-09-17 21:28:39 +0800

    dummy commit

--- test.cpp
+++ test.cpp
@@ -1,29 +1,68 @@
 #include <algorithm>
-#include <iostream>
 #include <vector>
-typedef long long ll;
+#include <chrono>
+#include <string>
+#include <random>
+#include <set>
 using namespace std;
 
-// brute force solution
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int n, q;
-    cin >> n >> q;
-    vector<int> w(n);
-    for (int &i : w) cin >> i;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
 
-    while (q--) {
-        int l, r, k;
-        cin >> l >> r >> k;
-        --l;
-        --r;
-        int mx = 0;
-        for (int a = l; a <= r; ++a) {
-            for (int b = a + 1; b <= r; ++b) {
-                if (w[a] > w[b]) mx = max(mx, w[a] + w[b]);
-            }
-        }
-        if (mx <= k) cout << "1\n";
-        else cout << "0\n";
-    }
+int _int(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+
+vector<int> _vector(int n, int l, int r) {
+	vector<int> res(n);
+	for (int i = 0; i < n; ++i)
+		res[i] = _int(l, r);
+	return res;
+}
+
+string _string(int n, char low_char = 'a', char high_char = 'z') {
+	string res(n, 'a');
+	for (int i = 0; i < n; ++i)
+		res[i] = _int(low_char, high_char);
+	return res;
+}
+
+vector<int> _permutation(int n) {
+	vector<int> res(n);
+	iota(res.begin(), res.end(), 1);
+	shuffle(res.begin(), res.end(), rng);
+	return res;
+}
+
+vector<pair<int, int>> _tree(int n) {
+	vector<pair<int, int>> edges;
+	vector<int> nodes(n);
+	iota(nodes.begin(), nodes.end(), 1);
+	shuffle(nodes.begin(), nodes.end(), rng);
+
+	for (int i = 1; i < n; ++i) {
+		int u = nodes[i];
+		int v = nodes[_int(0, i - 1)];
+		edges.emplace_back(u, v);
+	}
+	return edges;
+}
+
+vector<pair<int, int>> _graph(int n, int m, bool allow_self_loops = false, bool allow_multiple_edges = false) {
+	set<pair<int, int>> edge_set;
+	vector<pair<int, int>> edges;
+
+	while ((int)edges.size() < m) {
+		int u = _int(1, n);
+		int v = _int(1, n);
+		if (!allow_self_loops && u == v)
+			continue;
+
+		pair<int, int> e = minmax(u, v);
+		if (!allow_multiple_edges && edge_set.count(e))
+			continue;
+
+		edge_set.insert(e);
+		edges.push_back(e);
+	}
+	return edges;
 }
