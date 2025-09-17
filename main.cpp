da91c7c23e91a85685733afd683f6e949b39b98b
Batpurev0828 <sitoshi718@gmail.com>
2025-09-17 22:15:07 +0800

    prefix sum queries cses

--- main.cpp
+++ main.cpp
@@ -1,43 +1,35 @@
-<<<<<<< HEAD
-=======
 #include <algorithm>
 #include <cmath>
->>>>>>> 72e167d02ef3b52ed29816ce2b7f266c566f0e21
 #include <iostream>
 #include <queue>
 #include <set>
 #include <vector>
-<<<<<<< HEAD
-#include <algorithm>
-typedef long long ll;
-using namespace std;
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> b(n);
-    for (int &i : b) cin >> i;
-    
-}
-=======
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,sse4a,avx,avx2,popcnt,tune=native")
 typedef long long ll;
 using namespace std;
 
-struct Query {
-    int type, a, b;
+struct Node {
+    int64_t sum, pref;
+    Node() : sum(0), pref(0) {}
+    Node(int64_t _val) : sum(_val), pref(_val) {}
+    Node(int64_t _sum, int64_t _pref) : sum(_sum), pref(_pref) {}
+    Node operator+(const Node &b) const {
+        int64_t _sum = sum + b.sum;
+        int64_t _pref = max(pref, sum + b.pref);
+        return Node(_sum, max(_pref, (int64_t)0));
+    }
 };
 
 struct SegmentTree {
   private:
     int n;
-    vector<int> nums;
-    vector<int> tree;
+    vector<int64_t> nums;
+    vector<Node> tree;
     void build(int i, int L, int R) {
         if (L == R) {
-            tree[i] = nums[L];
+            tree[i] = Node(nums[L]);
             return;
         }
         int m = (L + R) / 2;
@@ -46,9 +38,9 @@ struct SegmentTree {
         build(y, m + 1, R);
         tree[i] = tree[x] + tree[y];
     }
-    void update(int i, int L, int R, int p, int v) {
+    void update(int i, int L, int R, int p, int64_t v) {
         if (L == R) {
-            tree[i] += v;
+            tree[i] = Node(v);
             return;
         }
         int m = (L + R) / 2;
@@ -60,7 +52,7 @@ struct SegmentTree {
         }
         tree[i] = tree[x] + tree[y];
     }
-    int query(int i, int L, int R, int l, int r) {
+    Node query(int i, int L, int R, int l, int r) {
         if (L == l && r == R) {
             return tree[i];
         }
@@ -71,88 +63,41 @@ struct SegmentTree {
         } else if (l > m) {
             return query(y, m + 1, R, l, r);
         } else {
-            int q1 = query(x, L, m, l, m);
-            int q2 = query(y, m + 1, R, m + 1, r);
+            Node q1 = query(x, L, m, l, m);
+            Node q2 = query(y, m + 1, R, m + 1, r);
             return q1 + q2;
         }
     }
 
   public:
-    SegmentTree(vector<int> &v) : n(v.size()), nums(v) {
-        tree.assign(4 * n, 0);
+    SegmentTree(vector<int64_t> &v) : n(v.size()), nums(v) {
+        tree.assign(4 * n, Node());
         build(0, 0, n - 1);
     }
+    Node query(int l, int r) { return query(0, 0, n - 1, l, r); }
     void update(int p, int v) { update(0, 0, n - 1, p, v); }
-    int query(int l, int r) { return query(0, 0, n - 1, l, r); }
 };
->>>>>>> 72e167d02ef3b52ed29816ce2b7f266c566f0e21
 
-int main(){
+int main() {
     cin.tie(0)->sync_with_stdio(0);
-<<<<<<< HEAD
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        solve();
-=======
     int n, q;
     cin >> n >> q;
-    vector<int> nums(n);
-    vector<int> c(n);
-    int mx = 0;
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
-        c[i] = nums[i];
-        mx = max(mx, nums[i]);
-    }
-    vector<Query> qs(q);
-    for (int i = 0; i < q; ++i) {
-        char t;
-        int a, b;
-        cin >> t >> a >> b;
-        if (t == '!') {
-            c.push_back(b);
-            qs[i] = {0, a, b};
+    vector<int64_t> nums(n);
+    for (int i = 0; i < n; ++i) cin >> nums[i];
+    SegmentTree tree(nums);
+    
+    while (q--) {
+        int type;
+        cin >> type;
+        if (type == 1) {
+            int p, v;
+            cin >> p >> v;
+            tree.update(p - 1, v);
         } else {
-            c.push_back(a);
-            c.push_back(b);
-            qs[i] = {1, a, b};
+            int l, r;
+            cin >> l >> r;
+            Node res = tree.query(l - 1, r - 1);
+            cout << max(res.pref, (int64_t)0) << '\n';
         }
-    }
-    sort(c.begin(), c.end());
-    c.resize(unique(c.begin(), c.end()) - c.begin());
-    // vector<int> freq(c.size() + 1, 0);
-    vector<int> freq(mx + 1, 0);
-    for (int &i : nums) {
-        // i = lower_bound(c.begin(), c.end(), i) - c.begin();
-        ++freq[i];
-    }
-
-    SegmentTree tree(freq);
-    for (int i = 0; i < freq.size(); ++i) {
-        cout << tree.query(i, i) << ' ';
-    }
-    cout << '\n';
-    for (Query &x : qs) {
-        if (x.type == 0) {
-            // x.b = lower_bound(c.begin(), c.end(), x.b) - c.begin();
-            int p = x.a - 1;
-            int v = x.b;
-            --p;
-            tree.update(nums[p], -1);
-            nums[p] = v;
-            tree.update(nums[p], 1);
-            for (int i = 0; i < freq.size(); ++i) {
-                cout << tree.query(i, i) << ' ';
-            }
-            cout << '\n';
-        } else {
-            // x.b = lower_bound(c.begin(), c.end(), x.b) - c.begin();
-            // x.a = lower_bound(c.begin(), c.end(), x.a) - c.begin();
-            int a = x.a;
-            int b = x.b;
-            cout << tree.query(a, b) << '\n';
-        }
->>>>>>> 72e167d02ef3b52ed29816ce2b7f266c566f0e21
     }
 }
