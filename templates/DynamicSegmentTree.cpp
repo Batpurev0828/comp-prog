3cd5bd51fd4c079afd2160c35476a71d22c693a6
Batpurev <sitoshi718@gmail.com>
2025-10-28 14:00:22 +0800

    dynamic segment tree template

--- templates/DynamicSegmentTree.cpp
+++ templates/DynamicSegmentTree.cpp
@@ -1,15 +1,3 @@
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <queue>
-#include <set>
-#include <vector>
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4.1,sse4.2,sse4a,avx,avx2,popcnt,tune=native")
-typedef long long ll;
-using namespace std;
-
 struct Node {
     int L, R, m, val;
     Node *x = nullptr;
@@ -64,5 +52,3 @@ struct SegmentTree {
         return root->query(l, r);
     }
 };
-
-int main() { cin.tie(0)->sync_with_stdio(0); }
